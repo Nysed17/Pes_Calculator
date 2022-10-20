0a31c09b9c7538f0e41c47a9759de58df7fd18ea
Nysed17 <nysedilsasso@gmail.com>
2022-10-20 21:09:41 +0200

    functions

---
+++ func.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+// factorial {
+std::vector<int> createFact(int n){
+   std::vector<int> res; 
+   for (int i = 1 ; i < n+1 ; i++){ //expliciting number
+      res.push_back(i);
+   }
+   return res;
+}
+
+float returnFact(int n){
+   std::vector<int> num = createFact(n); //copy
+   float res = 1;
+   for (int i = 0 ; i < n ; i++){
+      res *= num.at(i); //factorial
+   }
+   return res;
+}
+// }
