0db7b5e8fdd9794fc182dd6d19f9736e6c1d36eb
Nysed17 <nysedilsasso@gmail.com>
2022-10-21 10:22:44 +0200

    func

--- func.cpp
+++ func.cpp
@@ -1,20 +1,24 @@
 #include <vector>
+#include <iostream>
 
+class func{
 // factorial {
-std::vector<int> createFact(int n){
-   std::vector<int> res; 
-   for (int i = 1 ; i < n+1 ; i++){ //expliciting number
-      res.push_back(i);
+   public:      
+   std::vector<int> createFact(int n){
+      std::vector<int> res; 
+      for (int i = 1 ; i < n+1 ; i++){ //expliciting number
+         res.push_back(i);
+      }
+      return res;
    }
-   return res;
-}
 
-float returnFact(int n){
-   std::vector<int> num = createFact(n); //copy
-   float res = 1;
-   for (int i = 0 ; i < n ; i++){
-      res *= num.at(i); //factorial
+   float returnFact(int n){
+      std::vector<int> num = createFact(n); //copy
+      float res = 1;
+      for (int i = 0 ; i < n ; i++){
+         res *= num.at(i); //factorial
+      }
+      return res;
    }
-   return res;
-}
 // }
+};
