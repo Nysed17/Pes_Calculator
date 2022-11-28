dbb09c13b647bdf25d0d6a9898a50a72355442c2
Nysed17 <nysedilsasso@gmail.com>
2022-11-28 14:16:42 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "func.cpp"
+#include <cstdlib>
 #include <iostream>
+#include <math.h>
+#include <ostream>
 
 int select();
 
 class exec{
+   int n;
+   int k;
+   int p;
    public:
    void factorial(){
       func resf;
@@ -62,6 +68,38 @@ class exec{
       std::cout<<"\n\npress 1 for continue, 2 for quit: "; std::cin>>a;
       if (a == 1){select();}else{exit(0);}
    }
+
+   void binom(){
+      func resf;
+      system("clear");
+      std::cout<<"comb(n,k) * P^k * (1-P)^n-k\n\n"<<std::endl;
+      int n, k;
+      float p;
+      std::cout<<"number: "; std::cin>>n;
+      std::cout<<"k: "; std::cin>>k;
+      std::cout<<"probabilità: "; std::cin>>p;
+      std::cout<<"\n\nresult: "; float res = resf.returnFact(n) / (resf.returnFact(k) * resf.returnFact(n-k)) * pow(p,k) * pow(1-p,n-k); std::cout<<res;
+      int a;
+      std::cout<<"\n\npress 1 for continue, 2 for quit: "; std::cin>>a;
+      if (a == 1){select();}else{exit(0);}
+   }
+
+   void Ipergeometrica(){
+      func resf;
+      system("clear");
+      std::cout<<" comb(k,x) * comb(N - k, n - x)\n";
+      std::cout<<"-------------------------------\n";
+      std::cout<<"            comb(N,n)\n\n";
+      int n, k, N, x;
+      std::cout<<"n (numero di elementi presi in considerazione): "; std::cin>>n;
+      std::cout<<"k: "; std::cin>>k;
+      std::cout<<"N (numero totale di elementi): "; std::cin>>N;
+      std::cout<<"x (incognita): "; std::cin>>x;
+      std::cout<<"\n\nresult: "; float molt = (resf.returnFact(k) / (resf.returnFact(x) * resf.returnFact(k-x))) * (resf.returnFact(N-k) / (resf.returnFact(n-x) * resf.returnFact((N-k) - (n-x)))); float res = molt / (resf.returnFact(N) / (resf.returnFact(n) * resf.returnFact(N-n))); std::cout<<res;
+      int a;
+      std::cout<<"\n\npress 1 for continue, 2 for quit: "; std::cin>>a;
+      if (a == 1){select();}else{exit(0);}
+   }
 };
 
 int select(){
@@ -69,7 +107,7 @@ int select(){
    system("clear");
    std::cout<<"                                                 \n _____ _____            _         _     _            \n|  _  |   __|   ___ ___| |___ _ _| |___| |_ ___ ___  \n|   __|__   |  |  _| .'| |  _| | | | .'|  _| . |  _| \n|__|  |_____|  |___|__,|_|___|___|_|__,|_| |___|_|\n\n\n";
    int w;
-   std::cout<<"1] Factorial\n2] Factorial * Factorial\n3] Disposizione semplice\n4] Combinazione\n";
+   std::cout<<"1] Factorial\n2] Factorial * Factorial\n3] Disposizione semplice\n4] Combinazione\n5] Binomiale\n6] Ipergeometrica\n";
    std::cout<<"select: "; std::cin>>w;
    switch (w) {
       case 1:
@@ -83,6 +121,12 @@ int select(){
          break;
       case 4:
          on.combinazioni();
+         break;
+      case 5:
+         on.binom();
+         break;
+      case 6:
+         on.Ipergeometrica();
          break;
    }
    return 0;
